93e1184839c8873dbe3c4d5ecce68835a32c730d
mastercoldprince <54665402+mastercoldprince@users.noreply.github.com>
2023-12-13 17:21:02 +0800

    scaffoldv0.1

--- scaffold/before_run.py
+++ scaffold/before_run.py
@@ -7,17 +7,27 @@ from datetime import datetime
 parser = argparse.ArgumentParser(description='Backup or upload specific subdirectories of ditto.')
 parser.add_argument('-l', action='store_true', help='Backup specified subdirectories from server to local machine')
 parser.add_argument('-r', action='store_true', help='Backup specified subdirectories to remote server directory')
-parser.add_argument('-p', type=str, metavar='PATH', help='Upload specified subdirectory to remote home directory')
+parser.add_argument('-p', action='store_true' , help='Upload specified subdirectory to remote home directory')
 parser.add_argument('subdirectories', nargs='+', help='The subdirectories of ditto to backup or upload')
-
 # Parse the arguments
 args = parser.parse_args()
-
 # Define the server list
 server_list = [
     "node0",
 ]
 
+update_code_list = [
+    "node0",
+    "node1",
+    "node2",
+    "node3",
+    "node4",
+    "node5",
+    "node6",
+    "node7",
+    "node8",
+    "node9",
+        ]
 # User
 user = 'coldp'
 
@@ -32,16 +42,22 @@ def execute_command(command):
         print(f"Command failed with return code {e.returncode}: {e.cmd}")
         print(f"Error message: {e.stderr}")
 
+# Function to upload directories to servers
+def upload_directories(local_directory, remote_directory, servers):
+    for server in servers:
+        print(f"Uploading {local_directory} to {server}:{remote_directory}...")
+        execute_command(f"rsync -avz {local_directory} {user}@{server}:{remote_directory}")
+
 # Function to backup or upload subdirectories of ditto
 def handle_subdirectories(operation, subdirectories, local_path=None):
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
     for subdirectory in subdirectories:
         ditto_subdir_backup = f"ditto_{timestamp}"
-
-        for server in server_list:
-            remote_path = f"~/Ditto/{subdirectory}"
-            if operation == 'backup':
+        upload_remote_path = "~/Ditto/"
+        remote_path = f"~/Ditto/{subdirectory}"
+        if operation == 'backup':
+            for server in server_list:
                 if local_path:
                     # Backup to local machine
                     local_backup_path = os.path.join(local_path, ditto_subdir_backup)
@@ -52,9 +68,11 @@ def handle_subdirectories(operation, subdirectories, local_path=None):
                     remote_backup_path = f"~/{ditto_subdir_backup}"
                     print(f"Backing up {subdirectory} on {server} to {remote_backup_path}...")
                     execute_command(f"ssh {user}@{server} 'mkdir -p {remote_backup_path} && cp -r {remote_path} {remote_backup_path}'")
-            elif operation == 'upload':
-                print(f"Uploading {local_path} to {server}:{remote_path}...")
-                execute_command(f"scp -r {local_path} {user}@{server}:{remote_path}")
+        elif operation == 'upload':
+            for server in update_code_list:
+                print(f"Uploading {local_path} to {server}:{upload_remote_path}...")
+                # execute_command(f"scp -r {local_path} {user}@{server}:{remote_path}")
+                execute_command(f"rsync -avz {local_path} {user}@{server}:{upload_remote_path}")
 
 # Perform the backup to local or remote
 if args.l:
@@ -69,5 +87,5 @@ if args.r:
 # Upload the folders if the -p option is given
 if args.p:
     for subdirectory in args.subdirectories:
-        local_path = os.path.join(args.p, subdirectory)
+        local_path = subdirectory
         handle_subdirectories('upload', [subdirectory], local_path=local_path)
