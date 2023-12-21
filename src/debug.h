594b8eae5dc57a81cec9ff020ba407a9ebeb2268
mastercoldprince <54665402+mastercoldprince@users.noreply.github.com>
2023-12-21 17:18:11 +0800

    debug comment

--- src/debug.h
+++ src/debug.h
@@ -5,7 +5,7 @@
 #define L_INFO 1
 #define L_ERROR 2
 // NOTE : debug_mode, if not, comment the next line
-#define _DEBUG
+// #define _DEBUG
 #define STR_L_DEBUG "[DEBUG]"
 #define STR_L_INFO "[INFO]"
 #define STR_L_ERROR "[ERROR]"
