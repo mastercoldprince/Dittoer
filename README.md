4a30dcd4b8ba48478792d811af74af47f1b2342f
jim <623628963@qq.com>
2024-01-08 16:53:45 +0800

    local filter

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ditto: An Elastic and Adaptive Memory-Disaggregated Caching System
+the reappear to the project.
---
+++ scripts/setkey.py
@@ -0,0 +1,38 @@
+import os
+
+user = 'coldp'
+
+server_list = [
+    'node-0',
+    'node-1',
+    'node-2',
+    'node-3',
+    'node-4',
+    'node-5',
+    'node-6',
+    'node-7',
+    'node-8',
+    'node-9',
+]
+
+# generate keys
+for s in server_list:
+    cmd = f'ssh -o StrictHostKeyChecking=no {user}@{s} "ssh-keygen -t rsa -b 2048 -N \'\' -f ~/.ssh/id_rsa"'
+    os.system(cmd)
+
+pub_key_list = []
+for s in server_list:
+    cmd = f'scp -o StrictHostKeyChecking=no {user}@{s}:.ssh/id_rsa.pub ./'
+    os.system(cmd)
+    with open('id_rsa.pub', 'r') as f:
+        pub_key_list.append(f.readline())
+
+cmd = f'scp -o StrictHostKeyChecking=no {user}@{server_list[0]}:.ssh/authorized_keys ./'
+os.system(cmd)
+with open('authorized_keys', 'a') as f:
+    for key in pub_key_list:
+        f.write(key)
+
+for s in server_list:
+    cmd = f'scp -o StrictHostKeyChecking=no ./authorized_keys {user}@{s}:.ssh/authorized_keys'
+    os.system(cmd)
